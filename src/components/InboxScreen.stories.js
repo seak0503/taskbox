b92b0ac2ba2a6b51c4f6eb0d75fb5a8ac9d4ed4d
Shota Yamahira <shyamahira@bbtower.co.jp>
2022-06-15 17:32:00 +0900

    feat: 画面: API をモックする

--- src/components/InboxScreen.stories.js
+++ src/components/InboxScreen.stories.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { rest } from 'msw';
 
 import InboxScreen from './InboxScreen';
 import store from '../lib/store';
-
-const Template = () => <InboxScreen />;
+import { MockedState } from './TaskList.stories';
 
 export default {
   component: InboxScreen,
@@ -12,5 +12,32 @@ export default {
   decorators: [(story) => <Provider store={store}>{story()}</Provider>]
 };
 
+const Template = () => <InboxScreen />;
+
 export const Default = Template.bind({});
+Default.parameters = {
+  msw: {
+    handlers: [
+      rest.get(
+        'https://jsonplaceholder.typicode.com/todos?userId=1',
+        (req, res, ctx) => {
+          return res(ctx.json(MockedState.tasks));
+        }
+      )
+    ]
+  }
+};
+
 export const Error = Template.bind({});
+Error.parameters = {
+  msw: {
+    handlers: [
+      rest.get(
+        'https://jsonplaceholder.typicode.com/todos?userId=1',
+        (req, res, ctx) => {
+          return res(ctx.status(403));
+        }
+      )
+    ]
+  }
+};
