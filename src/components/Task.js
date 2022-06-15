8c5edadab6e8343b3b58fc07599f3095825f8f41
Shota Yamahira <shyamahira@bbtower.co.jp>
2022-06-15 10:44:16 +0900

    feat: 単純なコンポーネント: 状態を作り出す

--- src/components/Task.js
+++ src/components/Task.js
@@ -6,8 +6,41 @@ export default function Task({
   onPinTask
 }) {
   return (
-    <div className="list-item">
-      <input type="text" value={title} readOnly={true} />
+    <div className={`list-item ${state}`}>
+      <label className="checkbox">
+        <input
+          type="checkbox"
+          defaultChecked={state === 'TASK_ARCHIVED'}
+          disabled={true}
+          name="checked"
+        />
+        <span
+          className="checkbox-custom"
+          onClick={() => onArchiveTask(id)}
+          id={`archiveTask-${id}`}
+          area-label={`archiveTask-${id}`}
+        />
+      </label>
+      <div className="title">
+        <input
+          type="text"
+          value={title}
+          readOnly={true}
+          placeholder="Input title"
+        />
+      </div>
+
+      <div className="actions" onClick={(event) => event.stopPropagation()}>
+        {state !== 'TASK_ARCHIVED' && (
+          <a onClick={() => onPinTask(id)}>
+            <span
+              className={'icon-star'}
+              id={`pinTask-${id}`}
+              aria-label={`pinTask-${id}`}
+            />
+          </a>
+        )}
+      </div>
     </div>
   );
 }
