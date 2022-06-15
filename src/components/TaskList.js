65c0d768f423fcbb8d2d8a485a6941c947c0caa0
Shota Yamahira <shyamahira@bbtower.co.jp>
2022-06-15 11:28:53 +0900

    feat: 複合的なコンポーネント: データ要件とプロパティ

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Task from './Task';
 
@@ -55,3 +56,14 @@ export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
     </div>
   );
 }
+
+TaskList.propTypes = {
+  loading: PropTypes.bool,
+  tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
+  onPinTask: PropTypes.func,
+  onArchiveTask: PropTypes.func
+};
+
+TaskList.defaultProps = {
+  loading: false
+};
