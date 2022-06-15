878e571e669aa19a4c4023bfbbae7446cff64fae
Shota Yamahira <shyamahira@bbtower.co.jp>
2022-06-15 17:43:39 +0900

    test: 画面: Interactive stories

--- src/components/InboxScreen.stories.js
+++ src/components/InboxScreen.stories.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { rest } from 'msw';
+import {
+  within,
+  waitForElementToBeRemoved,
+  waitFor,
+  fireEvent
+} from '@storybook/testing-library';
 
 import InboxScreen from './InboxScreen';
 import store from '../lib/store';
@@ -26,6 +32,19 @@ Default.parameters = {
       )
     ]
   }
+};
+
+Default.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  // コンポーネントがロード状態から遷移するのを待ちます
+  await waitForElementToBeRemoved(await canvas.findByTestId('loading'));
+  // ストアに基づいてコンポーネントが更新されるのを待ちます
+  await waitFor(async () => {
+    //最初のタスクの固定をシミュレートします
+    await fireEvent.click(canvas.getByLabelText('pinTask-1'));
+    //番目のタスクの固定をシミュレートします
+    await fireEvent.click(canvas.getByLabelText('pinTask-3'));
+  });
 };
 
 export const Error = Template.bind({});
